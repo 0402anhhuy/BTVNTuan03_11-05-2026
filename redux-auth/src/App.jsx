dcb34548f7272d04af60e94cb4fa6fb78186c3c2
TranAnhHuy <anhhuyrubic@gmail.com>
2026-05-11 16:27:42 +0700

    Add App component with routing and authentication logic

---
+++ redux-auth/src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+
+const PrivateRoute = ({ children }) => {
+    const { isAuthenticated } = useSelector((state) => state.auth);
+    return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route
+                path="/profile"
+                element={
+                    <PrivateRoute>
+                        <Profile />
+                    </PrivateRoute>
+                }
+            />
+        </Routes>
+    );
+}
+
+export default App;
